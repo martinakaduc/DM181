00c34cd5e031b943fa89f843951183159c257630
Quang Duc <nguyenquangduc2000@gmail.com>
2018-11-19 12:20:57 +0700

    Fix bayes algorithm for multiple dimensions input

--- diabetesData/bayesClassifier.py
+++ diabetesData/bayesClassifier.py
@@ -89,7 +89,7 @@ class gaussianBayes(bayesClassic):
         self.y_proba = []
 
     def fit(self, X, y):
-        self.X_train = X.flatten()
+        self.X_train = np.array([x.flatten() for x in X])
         self.y_train = y
 
         #calculate probability of each y (P(y))
